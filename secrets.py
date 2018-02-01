0c4c6aea63109c00fcd8fde34711307840bf497b
Austin Topkis <atopkis@umich.edu>
2018-02-01 09:51:22 -0500

    jjj

---
+++ secrets.py
@@ -0,0 +1,7 @@
+nyt_key= 'e8b185ca11194a3cb41b867caaf05583'
+
+def get_stories(section):
+    baseurl = 'https://api.nytimes.com/svc/topstories/v2/'
+    extendedurl = baseurl + section + '.json'
+    params={'api-key': nyt_key}
+    return requests.get(extendedurl, params).json()
